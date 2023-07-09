373d42ab15f7013ab09d7d557504b0a43e33827c
DimkaYmka <mungalov00@icloud.com>
2023-07-10 00:10:54 +0900

    cors, hosts, - tokencheck

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const router = require('./routes');
 const handleError = require('./middlewares/handleError');
 
-const { PORT = 3000 } = process.env;
-const app = express();
 
+const app = express();
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+  maxAge: 30,
+}));
+const { PORT = 3000 } = process.env;
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-// });
-
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+// mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+//   useNewUrlParser: true,
+// });
 
 app.use(router);
 app.use(errors());
