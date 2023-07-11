bcd2f0b4cf5fd40fa4486f68e6265daef213a3d3
DimkaYmka <mungalov00@icloud.com>
2023-07-11 13:24:59 +0900

    server try

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -72,7 +72,8 @@ module.exports.login = (req, res, next) => {
             res.cookie('jwt', token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: true,
+              sameSite: 'none',
+              secure: true,
             });
             // Если совпадает -- вернуть пользователя
             res.send(user.toJSON());
