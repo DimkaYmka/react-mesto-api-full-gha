50fad5073f5ac2ce1fa170f6f7319f6717273d67
DimkaYmka <mungalov00@icloud.com>
2023-07-12 19:19:24 +0900

    samsite false

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -73,7 +73,7 @@ module.exports.login = (req, res, next) => {
             res.cookie('jwt', token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: true,
+              sameSite: false,
               secure: false,
             });
             // Если совпадает -- вернуть пользователя
