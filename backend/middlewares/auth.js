40e91a5e56d9a43990ac6bc91bed4ab25b8a8242
DimkaYmka <mungalov00@icloud.com>
2023-07-13 21:51:28 +0900

    auth error messange back

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -27,22 +27,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  // if (!authorization || !authorization.startsWith('Bearer '))  {
-  //   return next(new AuthError('Необходимо авторизоваться.'));
-  // }
   if (!authorization || !authorization.startsWith('Bearer '))  {
-    return next(new AuthError({ message: err.message }));
+    return next(new AuthError('Необходимо авторизоваться.'));
   }
-
+  
   const token = authorization.split('Bearer ')[1];
   let payload;
   try {
     payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-  //   return next(new AuthError('Необходимо авторизоваться.'));
-  // }
-  return next(new AuthError({ message: err.message }));
-}
+    return next(new AuthError('Необходимо авторизоваться.'));
+  }
 
   req.user = payload;
   return next();
