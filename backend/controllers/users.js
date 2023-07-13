5aadcb640367d4f9bc57a6a21a4ee6bdb0588f7b
DimkaYmka <mungalov00@icloud.com>
2023-07-13 16:13:35 +0900

    token back

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,6 +3,7 @@
 const bcrypt = require('bcryptjs');
 const jsonWebToken = require('jsonwebtoken');
 const userSchema = require('../models/user');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const NotFoundError = require('../errors/400');
 const AuthError = require('../errors/401');
@@ -69,13 +70,16 @@ module.exports.login = (req, res, next) => {
               _id: user._id,
             }, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
             // прикрепить его к куке
-            res.cookie('jwt', token, {
-              maxAge: 3600000,
-              httpOnly: true,
-              sameSite: true,
-            });
-            // Если совпадает -- вернуть пользователя
-            res.send(user.toJSON());
+            // res.cookie('jwt', token, {
+            //   maxAge: 3600000,
+            //   httpOnly: true,
+            //   sameSite: 'none',
+            //   secure: true,
+            // });
+            // // Если совпадает -- вернуть пользователя
+            // res.send(user.toJSON());
+            //return 
+            res.send({ token });
           } else {
             // Если не совпадает -- вернуть ошибку
             return next(new AuthError('Неправильный логин или пароль'));
--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,12 +1,18 @@
 const apiOptions = {
   // baseUrl: 'http://localhost:3000',
-  baseUrl:  'https://api.mesto.project.learn.nomoredomains.work/signin',
   // baseAuthUrl: 'https://auth.nomoreparties.co',
+  baseUrl: 'https://api.mesto.project.learn.nomoredomains.work',
   headers: {
-    authorization: 'bc440bce-88d2-40de-813f-9186a5211a71',
+    // authorization: 'bc440bce-88d2-40de-813f-9186a5211a71',
     'Content-Type': 'application/json'
   }
 };
+
+// authorization: `Bearer ${token}`,
+
+const getJWTByLocalStorage = () => {
+  return localStorage.getItem('jwt')
+}
 
 class Api {
   constructor(options) {
@@ -21,106 +27,134 @@ class Api {
       return res.json();
     }
     return Promise.reject(`Ошибка: ${res.status}`);
-}
+  }
 
   getInitialCards() {
+    // const token = getJWTByLocalStorage()
     return fetch(`${this._baseUrl}/cards`, {
       method: 'GET',
       credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+      }
       // headers: this._headers
     })
       .then(this._getResponse)
   }
 
 
-    // 
-    // getUserData() {
-    //   return fetch(`${this._baseUrl}/users/me`, {
-    //       method: 'GET',
-    //       headers: this._headers,
-    //     })
-    //     .then(this._getResponse)
-    // }
+  // 
+  // getUserData() {
+  //   return fetch(`${this._baseUrl}/users/me`, {
+  //       method: 'GET',
+  //       headers: this._headers,
+  //     })
+  //     .then(this._getResponse)
+  // }
 
-    getUserData() {
-      return fetch(`${this._baseUrl}/users/me`, {
-        method: 'GET',
-        credentials: 'include',
-        // headers: this._headers,
-      })
+  getUserData() {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+      }
+      // headers: this._headers,
+    })
       .then(this._getResponse)
-    }
-  
-    // 
-    editUserData(userInfo) {
-      return fetch(`${this._baseUrl}/users/me`, {
-          method: 'PATCH',
-          credentials: 'include',
-          headers: this._headers,
-          
-          body: JSON.stringify(userInfo)
-        })
-        .then(this._getResponse)
-    }
-// 
-    createCard(cardData) {
-      return fetch(`${this._baseUrl}/cards`,{
-        method: 'POST',
-        headers: this._headers,
-        credentials: 'include',
-        body: JSON.stringify(cardData)
-      })
+  }
+
+  // 
+  editUserData(userInfo) {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      credentials: 'include',
+
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(this._getResponse)
+  }
+  // 
+  createCard(cardData) {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(cardData)
+    })
       .then(this._getResponse);
-    }
+  }
 
-    changeLikeCardStatus(isLiked, cardId) {
-      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-        method: isLiked ? 'DELETE' : 'PUT',
-        credentials: 'include',
-        headers: this._headers,
-      })
+  changeLikeCardStatus(isLiked, cardId) {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: isLiked ? 'DELETE' : 'PUT',
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
+    })
       .then(this._getResponse);
-    }
+  }
 
-    // addLike(cardId) {
-    //   return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-    //     method: 'PUT',
-    //     headers: this._headers,
-    //     credentials: 'include',
-    //     // body: JSON.stringify(data)
-    //   })
-    //   .then(this._getResponse);
-    // }
-  
-    // deleteLike(cardId) {
-    //   return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-    //     method: 'DELETE',
-    //     headers: this._headers,
-    //     credentials: 'include',
-    //   })
-    //   .then(this._getResponse);
-    // }
+  // addLike(cardId) {
+  //   return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+  //     method: 'PUT',
+  //     headers: this._headers,
+  //     credentials: 'include',
+  //     // body: JSON.stringify(data)
+  //   })
+  //   .then(this._getResponse);
+  // }
 
-    editUserAvatar(avatarUrl) {
-      return fetch(`${this._baseUrl}/users/me/avatar`, {
-          method: 'PATCH',
-          credentials: 'include',
-          headers: this._headers,
-          body: JSON.stringify({ avatar: avatarUrl })
-        })
-        .then(this._getResponse);
-    }
+  // deleteLike(cardId) {
+  //   return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+  //     method: 'DELETE',
+  //     headers: this._headers,
+  //     credentials: 'include',
+  //   })
+  //   .then(this._getResponse);
+  // }
 
-
-
-    deleteCard(cardId) {
-      return fetch(`${this._baseUrl}/cards/${cardId}`, {
-        method: 'DELETE',
-        credentials: 'include',
-        headers: this._headers
-      })
+  editUserAvatar(avatarUrl) {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ avatar: avatarUrl })
+    })
       .then(this._getResponse);
-    }
+  }
+
+
+
+  deleteCard(cardId) {
+    // const token = getJWTByLocalStorage()
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(this._getResponse);
+  }
 
 
 }
