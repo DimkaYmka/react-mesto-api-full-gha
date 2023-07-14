ee1fa74d67eb5443a237ab8bcdec97c21018a2f1
DimkaYmka <mungalov00@icloud.com>
2023-07-14 20:29:34 +0900

    linter2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,6 +3,7 @@
 const bcrypt = require('bcryptjs');
 const jsonWebToken = require('jsonwebtoken');
 const userSchema = require('../models/user');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const NotFoundError = require('../errors/400');
@@ -69,16 +70,6 @@ module.exports.login = (req, res, next) => {
             const token = jsonWebToken.sign({
               _id: user._id,
             }, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-            // прикрепить его к куке
-            // res.cookie('jwt', token, {
-            //   maxAge: 3600000,
-            //   httpOnly: true,
-            //   sameSite: 'none',
-            //   secure: true,
-            // });
-            // // Если совпадает -- вернуть пользователя
-            // res.send(user.toJSON());
-            //return 
             return res.send({ token });
           } else {
             // Если не совпадает -- вернуть ошибку
